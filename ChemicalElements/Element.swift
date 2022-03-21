2482eb62eeed48b9a1fda7a89af1686f66d91dbe
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-21 19:26:20 -0400

    element struct added

---
+++ ChemicalElements/Element.swift
@@ -0,0 +1,18 @@
+//
+//  Element.swift
+//  ChemicalElements
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 21/3/22.
+//
+
+import Foundation
+
+struct element {
+    var atomicNumber: Int
+    var name: String
+    var symbol: String
+    var group: Int
+    var period: Int
+    var atomicWeight: Double
+    
+}
---
+++ ChemicalElements/chemicalElements.swift
@@ -0,0 +1,8 @@
+//
+//  chemicalElements.swift
+//  ChemicalElements
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 21/3/22.
+//
+
+import Foundation
