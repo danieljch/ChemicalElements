8f0e7c5558dda6aeb9804941f9cd197496b72649
Daniel JCH <danielj.callisayah@gmail.com>
2022-03-21 12:54:20 -0400

    Initial Commit

---
+++ ChemicalElements/ChemicalElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChemicalElementsApp.swift
+//  ChemicalElements
+//
+//  Created by Daniel Jesus Callisaya Hidalgo on 21/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ChemicalElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
